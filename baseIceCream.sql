7bb08a7b6acc1b2dea7f1aacb002d5f567704b7b
Blanca Malerva <blanca.malerva@gmail.com>
2018-03-12 23:38:52 -0600

    Modificación de la Base de datos

--- baseIceCream.sql
+++ baseIceCream.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE icecream;
 
 USE icecream;
-
+ -- Se crea una tabla en el cual se guardarán los datos del usuario
 CREATE TABLE Usuario(
     IdUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR (45),
-    apellido VARCHAR (45),
-    numero VARCHAR (10)
+    apellido VARCHAR (45) 
+     -- Se elimino el campo numero
 );
-
+ -- Se crea la tabla de helado
 CREATE TABLE helado(
 Id INT PRIMARY KEY AUTO_INCREMENT,
 sabor VARCHAR(45),
@@ -16,9 +16,9 @@ tipoCon VARCHAR(45),
 tipoHelado VARCHAR(45),
 tipoTam VARCHAR(45),
 Precio INT ,
-IdUsuario INT,
+IdUsuario INT, -- Se manda llamar el IdUsuario de la tabla Usuario
 INDEX (IdUsuario),
-FOREIGN KEY (IdUsuario) references Usuario (IdUsuario)
+FOREIGN KEY (IdUsuario) references Usuario (IdUsuario) -- Se le hace referencia como una llave foranea.
 
 );
 
